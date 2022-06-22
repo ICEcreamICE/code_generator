a94bf23d1668844dd3e918b0e0d4a81e793f4e1f
Gang.Wang <419016876@qq.com>
2022-06-22 09:25:48 +0800

    remove useless comments

--- flow/cg_class/LineParser.py
+++ flow/cg_class/LineParser.py
@@ -5,7 +5,6 @@ import json, os, re, sys, importlib
 
 class LineParser (OVERTURE):
   def __init__ (self, src_file, dst_file, dict):
-    #print ('init LineParser')
     self.stat = {}
     self.stat['RECORD_LINE_NUM'] = 1
     self.stat['VALID_IF']   = True
@@ -14,15 +13,9 @@ class LineParser (OVERTURE):
     self.stat['IF_NEST_LEVEL']   = 0
     self.stat['FOR_NEST_LEVEL']   = 0
     self.stat['LOOP']            = 'COMPLETED'
-      #'RECORD_LINE_NUM': 1 | 2 | 3 | ...
-      #'VALID_FOR_GEN'  : YES | NO
-      #'IF_CONDITION'   : IF | ELSIF | ELSE | ENDIF | None
-      #'IF_NEST_LEVEL'  : 0 | 1 | 2 | ...
-      #'LOOP'           : RUNNING | COMPLETED
     self.src_file = src_file
     self.dst_file = dst_file
     self.dict     = dict
-#    self.glbs = Globalizer()
 
   def if_branch(self, command, eval_content, line):
     _if_line = ''
@@ -32,12 +25,10 @@ class LineParser (OVERTURE):
     _SpaceValue = 0
     
     _TotalSpaceValue = int(self.stat['IF_NEST_LEVEL'] + self.stat['FOR_NEST_LEVEL'])
-    #print (command)
     if int(_TotalSpaceValue) > 0:
       _ifSpaceValue = _TotalSpaceValue
       _SpaceValue = int(_TotalSpaceValue - 2)
 
-#    _SpaceValue = _SpaceValue + int(self.stat['FOR_NEST_LEVEL'])
     if command == 'IF':
       _eval_content = '' + eval_content + ':'
       _if_line = line + _space* _ifSpaceValue + 'if ' + _eval_content + '\n'
@@ -54,14 +45,10 @@ class LineParser (OVERTURE):
       self.stat['IF_NEST_LEVEL'] = self.stat['IF_NEST_LEVEL'] + 2 #TODO add ID to suggest different command, IF/FOR
     elif (command == 'ENDIF'):
       self.stat['IF_NEST_LEVEL'] = self.stat['IF_NEST_LEVEL'] - 2
-
-    #line_number += 1
     return _if_line
-#    self.stat[line_number].update({'VALID_IF': if_result})
 
 #TODO: Not close FOR-ENDFOR loop will not be notified! comment ``FOR loop has bug (//``FOR)
   def for_loop(self, command, eval_content, line):
-    #print (eval_content)
     _tmp_search = re.search ("\s?(\w+)\s?in\s?(.+)\s?", eval_content)
     _space = ' '
     _for_line = ''
@@ -88,10 +75,6 @@ class LineParser (OVERTURE):
 
   def sub_func(self, being_matched):
     _qoute = self.QuoteStyleSelection(being_matched)
-    # if re.search('"', being_matched):
-    #   _qoute = "'''"
-    # else:
-    #   _qoute = '"""'
     being_matched = being_matched.strip('\n')
     _string_size = len(being_matched)
     _tmp = re.finditer ("``{([^}]*)}", being_matched)
@@ -100,7 +83,6 @@ class LineParser (OVERTURE):
     _variable_list = []
     _first_iter = True
     _write_string = ''
-    #print (_string_size)
     _tmp = list(_tmp) #_tmp type changed from iter to list CAUTION TODO: Fix it later
     if _tmp:
       for _value in list(_tmp):
@@ -142,19 +124,7 @@ class LineParser (OVERTURE):
       _value_list.append('')
       _start_stop_list.append(_value_list)
 
-    #print(len(being_matched))
-    #print (being_matched[0:11])
-    #print (being_matched[14:25])
-    #print (being_matched[28:34])
-    #print (being_matched[37:38])
-    #print ("END")
-    #print (_start_stop_list)
-    #print (being_matched)
     for _sstart, _send, _vstart, _vend in _start_stop_list:
-      #print (_sstart)
-      #print (_send)
-      #print (_vstart)
-      #print (_vend)
       _svariable = 'being_matched' + '[' +  str(_sstart) + ':' + str(_send) + ']'
       if (_vstart != ''):
         _variable = 'being_matched[' +  str(_vstart) + ':' + str(_vend) + ']'
@@ -167,16 +137,11 @@ class LineParser (OVERTURE):
         _plus_string = ''
         _plus_string_left_enbrace = ''
       _svariable = eval(_svariable)
-      #print(_svariable)
       _write_string = (_write_string
                        + _qoute + _svariable  + _qoute
                        + _plus_string
                        + _variable
                        + _plus_string_left_enbrace)
-      #print (_write_string)
-      #_sub_line = _sub_line + being_matched[
-    #print (_write_string)
-    #exit()
     return _write_string
 
   def variable_sub(self, command, eval_content, line):
@@ -196,7 +161,7 @@ class LineParser (OVERTURE):
     _writeString = ''
     _qoute = self.QuoteStyleSelection(line)
     _searchTriApostrophe = '\s?```(.+)'
-    _searchTriApoString = '\s?```p((\s?)+)(.+)'
+    _searchTriApoString = '\s?````p((\s?)+)(.+)'
     _result = re.search(_searchTriApostrophe, line)
     _resultString = re.search(_searchTriApoString, line)
     # _writeString = " ".join([_qoute, _result.group(1), _qoute])
@@ -208,10 +173,6 @@ class LineParser (OVERTURE):
 
   def Parser(self):
     MINE = self.dict
-    #print ('parsing...')
-    #for f in func_list:
-    #  TEST_COND = self.f()
-    #print (self.TEST_COND())
     parsing_file = self.src_file
     _space = ' '
     line_number  = 0
@@ -220,24 +181,15 @@ class LineParser (OVERTURE):
     search_elsif   = '.?``ELSIF\s?{(.+)}(.+)?'
     search_else    = '.?``ELSE(.+)?'
     search_endif   = '.?``ENDIF(.+)?'
-#    search_for     = '``FOR\s?{\s?(.+)\s?}\s?{\s?(.+)\s?}\s?{\s?(.+)\s?}' 
     search_for     = '.?``FOR\s?{\s?(.+)\s?}\s?(.+)?'
-#    search_for     = '.?(?<!//)``FOR\s?{\s?(.+)\s?}\s?(.+)?'
     search_endfor  = '.?``ENDFOR(.+)?'
-    #search_content = '^.+$'
     search_content = '```'
-    #_InitialFunction =  'bui'+'ltins.X'+'Y' + 'T = ' + str(MINE) + '\n' #+'print("==================", CODEGEN)\n'
-    #_InitialImport = 'import sys,bu' + 'iltins\n' + _InitialFunction
-    #_dst_file_content = 'import sys \nsys.path.append ("' + self.dict['CF_DIR']  +'")\nfrom CF import *\n'
-    #_dst_file_content = '\"\"\"\n'
     _dst_file_content = ''
     #exec (_InitialImport)
     with open (self.src_file, 'r') as parsing_file:
-      # sys.path.append (self.dict['CF_DIR']) 
       sys.path.append("./RTL_CODE") 
       _Cfg = importlib.import_module ('CONF')
       file_lines = parsing_file.readlines()
-      #while (line_number < len(file_lines)):
       for line in file_lines:
         ResultCfg    = re.search(SearchCfg,    line)
         result_if    = re.search(search_if,    line)
@@ -266,31 +218,24 @@ class LineParser (OVERTURE):
           continue
         else:
           if (result_if != None):
-            #print ('1')
             eval_content = result_if.group(1)
             eval_content = self.SubstituteFunctionName(eval_content)
             _dst_file_content = self.if_branch('IF', eval_content, _dst_file_content)
           elif (result_elsif != None):
-            #print ('2')
             eval_content = result_elsif.group(1)
             eval_content = self.SubstituteFunctionName(eval_content)
             _dst_file_content = self.if_branch('ELSIF', eval_content, _dst_file_content)
           elif (result_else != None):
-            #print ('3')
             _dst_file_content = self.if_branch('ELSE', 'False', _dst_file_content)
           elif (result_endif != None):
-            #print ('4')
             _dst_file_content = self.if_branch('ENDIF', 'False', _dst_file_content)
           elif (result_for != None):
-            #print ('5')
             eval_content = result_for.group(1)
             eval_content = self.SubstituteFunctionName(eval_content)
             _dst_file_content = self.for_loop('FOR', eval_content, _dst_file_content)
           elif (result_endfor != None):
-            #print ('6')
             _dst_file_content = self.for_loop('ENDFOR', 'False', _dst_file_content)
           elif (result_content != None):
-            #print ('7')
             line = self.ParsePythonCommand(line)
             _dst_file_content = (_dst_file_content
                                  + _space * (int(self.stat['IF_NEST_LEVEL']) + int(self.stat['FOR_NEST_LEVEL']))
@@ -300,7 +245,6 @@ class LineParser (OVERTURE):
           else:
             if re.search ('``', line) != None:
               #TODO: causion this line may has \n as blow condition
-              #print ('line: ', line)
               line = self.sub_func(line)
               _dst_file_content = (_dst_file_content
                                    + _space * (int(self.stat['IF_NEST_LEVEL']) + int(self.stat['FOR_NEST_LEVEL']))
@@ -316,17 +260,11 @@ class LineParser (OVERTURE):
                                    + " \"\"\"" 
                                    + "+\"\"\"\\n\"\"\")\n"
                                    )
-            # print (_dst_file_content)
             line_number += 1
-#    _dst_file_content = _dst_file_content + '\n' +'\"\"\"'
     parsing_file.close()
     #print (self.dst_file)
     with open (self.dst_file, 'w') as output_content:
-#      for lines in _dst_file_content:
-#        print (lines + '\n')
-#        output_content.write (lines)
-#      _dst_file_content = """output_content.write ("i2c_slv #(.I2C_SLV_ADR_0(7'b111_0000), .I2C_SLV_ADR_1(7'b111_0100)) inst_i2c_slv (\\n")"""
-      print (_dst_file_content)
+      #print (_dst_file_content)
       if self.stat['FOR_NEST_LEVEL'] != 0:
         print ("Unmatched ``FOR-``ENDFOR pair, please check!")
         exit(1)
